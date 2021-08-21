eb96fb798097a5a120dc0d56a8337e6122f7e8f6
SdMusic <peter_freemantle@hotmail.com>
2021-08-21 14:14:06 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JDTZuKJLyYKB5lvAXvTxsVXQylZPgWKTbu6TXkZSw3TXAp532Pet4Y8dZHauKJ2ybgIESItezw7RaB9MK7OXoJX00XD2mUWTw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
