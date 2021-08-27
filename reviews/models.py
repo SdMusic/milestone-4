22399b640d5f13e693c85baff404628a17291b33
SdMusic <peter_freemantle@hotmail.com>
2021-08-27 11:41:46 +0000

    reviews form.py added

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Review(models.Model):
+    """
+    Review model to allow user to leave a product review
+    """
+    RATING = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rating = models.IntegerField(choices=RATING)
+
+    def __str__(self):
+        return self.title
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def reviews(request):
     return render(request, 'reviews/review.html')
