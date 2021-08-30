ec48cbc1683f8284591da8df40b9478951d4d689
SdMusic <peter_freemantle@hotmail.com>
2021-08-30 14:54:30 +0000

    xframes test for am i respsive picture

--- tipples/settings.py
+++ tipples/settings.py
@@ -68,6 +68,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ROOT_URLCONF = 'tipples.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
